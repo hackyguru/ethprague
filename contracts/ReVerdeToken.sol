2d32c00b998e96d2e9e9ae67ba8b0b79d03167a6
Tranquil-Flow <66773372+Tranquil-Flow@users.noreply.github.com>
2024-06-01 19:34:01 +0200

    add withdraw functions for stuck tokens/coins

--- contracts/ReVerdeToken.sol
+++ contracts/ReVerdeToken.sol
@@ -9,9 +9,12 @@ contract ReVerdeToken is ERC20, Ownable {
     IERC20 public usdcToken;
     address public treasuryAddress;
 
-    error inputAmountMustBeGreaterThanZero();
-    error tokenInputTransferFailed();
-    error tokenOutputTransferFailed();
+    error TokenInputTransferFailed();
+    error TokenOutputTransferFailed();
+    error AmountMustBeGreaterThanZero();
+    error InsufficientBalance();
+    error TokenTransferFailed();
+    error CoinTransferFailed();
 
     constructor(address _usdcTokenAddress, address _treasuryAddress) ERC20("ReVerde", "RVD") Ownable(msg.sender) {
         usdcToken = IERC20(_usdcTokenAddress);
@@ -19,21 +22,37 @@ contract ReVerdeToken is ERC20, Ownable {
     }
 
     function depositUSDC(uint256 _amount) external {
-        if (_amount <= 0) {revert inputAmountMustBeGreaterThanZero();}
+        if (_amount <= 0) {revert AmountMustBeGreaterThanZero();}
 
         // Transfer USDC from user to this contract
         bool sent = usdcToken.transferFrom(msg.sender, address(this), _amount);
-        if (!sent) {revert tokenInputTransferFailed();}
+        if (!sent) {revert TokenInputTransferFailed();}
 
         // Mint ReVerde tokens to user at a 1:1 ratio
         _mint(msg.sender, _amount);
 
-        // Transfer USDC to Gitcoin address
+        // Transfer USDC to treasury address
         bool transferred = usdcToken.transfer(treasuryAddress, _amount);
-        if (!transferred) {revert tokenOutputTransferFailed();}
+        if (!transferred) {revert TokenOutputTransferFailed();}
     }
 
-    function updatetreasuryAddress(address _newAddress) external onlyOwner {
+    function updateTreasuryAddress(address _newAddress) external onlyOwner {
         treasuryAddress = _newAddress;
     }
+
+    function withdrawERC20(address _tokenAddress, uint256 _amount) external onlyOwner {
+        if (_amount <= 0) {revert AmountMustBeGreaterThanZero();}
+        IERC20 token = IERC20(_tokenAddress);
+        bool sent = token.transfer(msg.sender, _amount);
+        if (!sent) {revert TokenTransferFailed();}
+    }
+
+    function withdrawEther(uint256 _amount) external onlyOwner {
+        if (_amount <= 0) {revert AmountMustBeGreaterThanZero();}
+        if (address(this).balance < _amount) {revert InsufficientBalance();}
+        (bool sent, ) = msg.sender.call{value: _amount}("");
+        if (!sent) {revert CoinTransferFailed();}
+    }
+
+    receive() external payable {}
 }
